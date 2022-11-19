abebc915e5fd2e686ab3565808c701845ff3ed90
Sharath Kumar <shark.krish@gmail.com>
2022-11-19 10:10:48 +0530

    first commit

---
+++ reverseString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter string");
+            string str = Console.ReadLine();
+            Console.WriteLine("String before reversing is -" + str);
+            ReverseString(str);
+            Console.ReadLine();
+        }
+
+        internal static void ReverseString(string str)
+        {
+            char[] charArray = str.ToCharArray();
+
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                charArray[i] = str[j];
+                charArray[j] = str[i];
+            }
+            string reversedString = new string(charArray);
+            Console.WriteLine("String after reversing is "+ reversedString);
+        }
+    }
+}
